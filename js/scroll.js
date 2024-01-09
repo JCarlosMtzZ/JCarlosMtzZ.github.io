c97e01e73a8f3d03b8d3f23c602ee80ae134a213
JCarlosMtzZ <juancmtz777@gmail.com>
2024-01-08 18:31:32 -0600

    footer and navbar icons

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,15 @@
+window.addEventListener('beforeunload', function () {
+    window.scrollTo(0, 0);
+});
 document.getElementById('scroll-down').addEventListener('click', function() {
     window.scrollBy({
         top: window.innerHeight,
+        behavior: 'smooth'
+    });
+});
+document.getElementById('scroll-up').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
         behavior: 'smooth'
     });
 });
