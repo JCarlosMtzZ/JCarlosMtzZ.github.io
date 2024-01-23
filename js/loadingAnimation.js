da77415f19411efaee4c45e8265fa855df31f43e
JCarlosMtzZ <juancmtz777@gmail.com>
2024-01-22 18:23:57 -0600

    small fixes and loading animation

---
+++ js/loadingAnimation.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+    const loader = document.querySelector('.loader');
+    const body = document.body;
+
+    setTimeout(() => {
+        loader.classList.add('loader--hidden');
+        loader.addEventListener('transitionend', () => {
+            body.style.overflow = 'auto';
+            body.removeChild(loader);
+        })
+    }, 2000);
+});
--- js/scroll.js
+++ js/scroll.js
@@ -1,15 +1,15 @@
-//window.addEventListener('beforeunload', function () {
-//    window.scrollTo(0, 0);
-//});
-document.getElementById('scroll-down').addEventListener('click', function() {
-    window.scrollBy({
-        top: window.innerHeight,
-        behavior: 'smooth'
-    });
+window.addEventListener('beforeunload', function () {
+    window.scrollTo(0, 0);
 });
 document.getElementById('scroll-up').addEventListener('click', function() {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
+});
+document.getElementById('scroll-down').addEventListener('click', function() {
+    var element = document.querySelector('#about');
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
 });
